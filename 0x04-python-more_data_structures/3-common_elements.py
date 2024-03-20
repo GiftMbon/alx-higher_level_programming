f602d02dad8567d8323deefa044036ed671a1ef2
Gift Mbonani <giftmbonani@outlook.com>
2024-03-20 03:46:40 -0700

    task3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-uniq_add = __import__('2-uniq_add').uniq_add
 
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
+def common_elements(set_1, set_2):
+    return set_1.intersection(set_2)
